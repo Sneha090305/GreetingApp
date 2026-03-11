94a642b633c3097374b246d300f3a414659fea00
Sneha <snehangap@gmail.com>
2026-03-11 11:12:46 +0530

    [Sneha] : UC2: Implemented GreetingService and moved greeting logic to service layer

--- src/main/java/com/example/greetingapp/GreetingappApplication.java
+++ src/main/java/com/example/greetingapp/GreetingappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreetingappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreetingappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingappApplication.class, args);
+    }
 
 }
